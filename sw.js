87fcea4ead7a2005e78e07243536a9926ae77fb6
David-beep-lab <davidsuper.2018@gmail.com>
2026-03-22 02:14:50 +0200

    Disable service worker and add activation log
    Temporarily disable service worker functionality and log a message on activation.

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-// Minimal Service Worker for PWA
-const CACHE = 'startuphub-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'css/style.css', 'js/app.js', 'js/storage.js', 'js/i18n.js'])));
+// Service Worker disabled temporarily
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker disabled');
 });
