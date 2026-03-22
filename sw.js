5f78196066c7d063e08807fb3fe87e7a6a5383f0
David-beep-lab <davidsuper.2018@gmail.com>
2026-03-22 02:35:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-// Service Worker disabled temporarily
-self.addEventListener('install', event => {
+// Minimal Service Worker for PWA
+const CACHE = 'startuphub-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'css/style.css', 'js/app.js', 'js/storage.js', 'js/i18n.js'])));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker disabled');
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
